ae0f6bf7e0d8e10fd2b2165fe841231a4562cbe5
jason-zhang-github <zhang.jason.2021@gmail.com>
2021-10-27 23:26:11 -0400

    changed booklist

--- app/src/main/java/com/example/audiobb/BookList.kt
+++ app/src/main/java/com/example/audiobb/BookList.kt
@@ -1,8 +1,10 @@
 package com.example.audiobb
 
-class BookList (book_: Book){
+class BookList()
+{
 
     var books : ArrayList<Book> = arrayListOf()
+
     fun add(book : Book)
     {
         books.add(book)
