f3e771c2da2e149aca62e11c1d292766204142a7
jason-zhang-github <zhang.jason.2021@gmail.com>
2021-10-27 23:25:29 -0400

    created booklist

---
+++ app/src/main/java/com/example/audiobb/BookList.kt
@@ -0,0 +1,26 @@
+package com.example.audiobb
+
+class BookList (book_: Book){
+
+    var books : ArrayList<Book> = arrayListOf()
+    fun add(book : Book)
+    {
+        books.add(book)
+    }
+
+    fun remove(book : Book)
+    {
+        books.remove(book)
+    }
+
+    fun get(index : Int) : Book
+    {
+        return books.get(index)
+    }
+
+    fun size() : Int
+    {
+        return books.size
+    }
+
+}
